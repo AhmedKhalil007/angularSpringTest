a20807f85051836cb8616b5e8112a67ea3b29f42
AhmedKhalil007 <letaief.ahmedkhalil@gmail.com>
2023-11-18 21:08:29 +0100

    third

--- src/app/services/personne.service.ts
+++ src/app/services/personne.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Personne } from '../models/personne.model'; 
 
@@ -11,7 +11,14 @@ export class PersonneService {
   constructor(private http: HttpClient) { }
 
   ajouterPersonne(personne: Personne): Observable<any> {
-    return this.http.post<any>('http://localhost:7090/personnes/savePersonne', personne);
+    // Ajoutez les en-têtes pour accepter le JSON
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      })
+    };
+
+    return this.http.post<any>('http://localhost:7090/personnes/savePersonne', personne, httpOptions);
   }
-  
 }
